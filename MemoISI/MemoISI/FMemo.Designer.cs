9d60660891972b83018a01a0067bf6e8d0b21842
Jérôme Alibert <jeromealibert96@gmail.com>
2017-01-23 18:00:07 +0100

    Init projet

---
+++ MemoISI/MemoISI/FMemo.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace MemoISI
+{
+    partial class FMemo
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PB_1 = new System.Windows.Forms.PictureBox();
+            this.PB_2 = new System.Windows.Forms.PictureBox();
+            this.PB_3 = new System.Windows.Forms.PictureBox();
+            this.PB_4 = new System.Windows.Forms.PictureBox();
+            this.PB_5 = new System.Windows.Forms.PictureBox();
+            this.PB_6 = new System.Windows.Forms.PictureBox();
+            this.PB_7 = new System.Windows.Forms.PictureBox();
+            this.PB_8 = new System.Windows.Forms.PictureBox();
+            this.PB_ToFind = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_ToFind)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PB_1
+            // 
+            this.PB_1.Location = new System.Drawing.Point(12, 87);
+            this.PB_1.Name = "PB_1";
+            this.PB_1.Size = new System.Drawing.Size(125, 125);
+            this.PB_1.TabIndex = 0;
+            this.PB_1.TabStop = false;
+            // 
+            // PB_2
+            // 
+            this.PB_2.Location = new System.Drawing.Point(152, 87);
+            this.PB_2.Name = "PB_2";
+            this.PB_2.Size = new System.Drawing.Size(125, 125);
+            this.PB_2.TabIndex = 1;
+            this.PB_2.TabStop = false;
+            // 
+            // PB_3
+            // 
+            this.PB_3.Location = new System.Drawing.Point(292, 87);
+            this.PB_3.Name = "PB_3";
+            this.PB_3.Size = new System.Drawing.Size(125, 125);
+            this.PB_3.TabIndex = 2;
+            this.PB_3.TabStop = false;
+            // 
+            // PB_4
+            // 
+            this.PB_4.Location = new System.Drawing.Point(435, 87);
+            this.PB_4.Name = "PB_4";
+            this.PB_4.Size = new System.Drawing.Size(125, 125);
+            this.PB_4.TabIndex = 3;
+            this.PB_4.TabStop = false;
+            // 
+            // PB_5
+            // 
+            this.PB_5.Location = new System.Drawing.Point(12, 218);
+            this.PB_5.Name = "PB_5";
+            this.PB_5.Size = new System.Drawing.Size(125, 125);
+            this.PB_5.TabIndex = 4;
+            this.PB_5.TabStop = false;
+            // 
+            // PB_6
+            // 
+            this.PB_6.Location = new System.Drawing.Point(152, 218);
+            this.PB_6.Name = "PB_6";
+            this.PB_6.Size = new System.Drawing.Size(125, 125);
+            this.PB_6.TabIndex = 5;
+            this.PB_6.TabStop = false;
+            // 
+            // PB_7
+            // 
+            this.PB_7.Location = new System.Drawing.Point(292, 218);
+            this.PB_7.Name = "PB_7";
+            this.PB_7.Size = new System.Drawing.Size(125, 125);
+            this.PB_7.TabIndex = 6;
+            this.PB_7.TabStop = false;
+            // 
+            // PB_8
+            // 
+            this.PB_8.Location = new System.Drawing.Point(435, 218);
+            this.PB_8.Name = "PB_8";
+            this.PB_8.Size = new System.Drawing.Size(125, 125);
+            this.PB_8.TabIndex = 7;
+            this.PB_8.TabStop = false;
+            // 
+            // PB_ToFind
+            // 
+            this.PB_ToFind.Location = new System.Drawing.Point(435, 375);
+            this.PB_ToFind.Name = "PB_ToFind";
+            this.PB_ToFind.Size = new System.Drawing.Size(125, 125);
+            this.PB_ToFind.TabIndex = 8;
+            this.PB_ToFind.TabStop = false;
+            // 
+            // FMemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 556);
+            this.Controls.Add(this.PB_ToFind);
+            this.Controls.Add(this.PB_8);
+            this.Controls.Add(this.PB_7);
+            this.Controls.Add(this.PB_6);
+            this.Controls.Add(this.PB_5);
+            this.Controls.Add(this.PB_4);
+            this.Controls.Add(this.PB_3);
+            this.Controls.Add(this.PB_2);
+            this.Controls.Add(this.PB_1);
+            this.Name = "FMemo";
+            this.Text = "Memory";
+            ((System.ComponentModel.ISupportInitialize)(this.PB_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_ToFind)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PB_1;
+        private System.Windows.Forms.PictureBox PB_2;
+        private System.Windows.Forms.PictureBox PB_3;
+        private System.Windows.Forms.PictureBox PB_4;
+        private System.Windows.Forms.PictureBox PB_5;
+        private System.Windows.Forms.PictureBox PB_6;
+        private System.Windows.Forms.PictureBox PB_7;
+        private System.Windows.Forms.PictureBox PB_8;
+        private System.Windows.Forms.PictureBox PB_ToFind;
+    }
+}
+
---
+++ MemoISI/MemoISI/FMemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoISI
+{
+    public partial class FMemo : Form
+    {
+        public FMemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
